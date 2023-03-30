1c58d0da00f1dc53529caa6df9ad033a5212d47a
KMchaudhari7995 <kmchaudhari7912@gmail.com>
2023-03-31 00:30:11 +0100

    My Java Homework week 7

---
+++ src/Homework_week_7/Programme_1_OddEvenTernaryOperator.java
@@ -0,0 +1,22 @@
+package Homework_week_7;
+
+import java.util.Scanner;
+
+public class Programme_1_OddEvenTernaryOperator {
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int number =scanner.nextInt();
+        isItOddOrEvenNUmber(number);
+        //closing the scanner object
+        scanner.close();
+    }
+    //checking the number is even or odd
+    public static void isItOddOrEvenNUmber(int number){
+        //ternary operator is used
+        String evenOrOdd = (number %2 ==0) ? "Even": "Odd";
+        System.out.println("The"+ number + "is"+ evenOrOdd +"number");
+    }
+
+}
